ff6b978e6245b7277a2f491c938e2179d1edcf1f
cybertronk58-dot <cybertronk58@gmail.com>
2026-01-30 15:06:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dmt4oxkcz/video/upload/v1769814321/Jorge_Rivera-Herrans_Anna_Lea_-_Would_You_Fall_In_Love_With_Me_Again_Lyrics_tiesaq.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
